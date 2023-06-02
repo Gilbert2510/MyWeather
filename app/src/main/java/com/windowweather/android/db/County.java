d8c127f901420bdbe4cc262bbef9618ec9fec9e0
Gilbert2510 <1023161984@qq.com>
2023-06-02 11:07:33 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/windowweather/android/db/County.java
@@ -0,0 +1,40 @@
+package com.windowweather.android.db;
+
+public class County {   //县（区）
+    private int id;     //县（区）ID
+    private String countyName;      //县（区）名
+    private int weatherId;          //天气ID
+    private int cityId;             //所属市ID
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/windowweather/android/db/Province.java
@@ -0,0 +1,31 @@
+package com.windowweather.android.db;
+
+public class Province {     //省
+    private int id;     //省ID
+    private String provinceName;    //省名
+    private int provinceCode;       //省代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
